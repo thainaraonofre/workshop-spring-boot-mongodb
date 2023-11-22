620e22c7cc74e2a6ca884a41c0cd0731e85517d6
Thainara Onofre <thainaraonofre@outlook.com>
2023-11-22 16:08:06 -0300

    Initial commit

---
+++ src/main/java/projectmongodb/workshopspringbootmongodb/WorkshopSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package projectmongodb.workshopspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectmongodb/workshopspringbootmongodb/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package projectmongodb.workshopspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
